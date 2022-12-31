c246c80d9c2ef43dc3ebcaa8676ec23f8571428a
yuy@cmdm197 <yuy@cmdm.csie.ntu.edu.tw>
2022-12-31 18:12:36 +0800

    update summary table of files

--- src/app/controllers/dashboard_controller.rb
+++ src/app/controllers/dashboard_controller.rb
@@ -3,9 +3,12 @@ class DashboardController < ApplicationController
 
     before_action :authenticate_user!
     include HandParser
+
     def index
         @user_id = current_user.id
+        @uploads = current_user.uploads
         hand_pos_name()
     end
+    
 
 end
--- src/app/controllers/uploads_controller.rb
+++ src/app/controllers/uploads_controller.rb
@@ -23,13 +23,33 @@ class UploadsController < ApplicationController
         redirect_to controller: 'uploads', action: 'new', hand_pos: upload_params[:hand_pos]
     end
 
+    def show
+        if user_files_accessible_check
+            @upload = Upload.find(params[:id])
+        else
+            flash[:notice] = "You are not allowed to operate this file."
+            redirect_to status_path
+        end
+    end
+
     def destroy
-        @upload.destroy
-        redirect_to dashboard_index_path
+        if user_files_accessible_check
+            @upload = Upload.find(params[:id])
+            @upload.destroy
+            redirect_to controller: 'dashboard', action: 'index', hand_pos: @upload[:hand_pos]
+        else
+            flash[:notice] = "You are not allowed to operate this file."
+            redirect_to status_path
+        end
     end
 
     private
     def upload_params
         params.require(:upload).permit(:avatar, :hand_LR, :hand_pos)
+    end
+    
+    def user_files_accessible_check
+        @current_user_uploads_ids = current_user.uploads.ids
+        return @current_user_uploads_ids.include? params[:id].to_i
     end
 end
