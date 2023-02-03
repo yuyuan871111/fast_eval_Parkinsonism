4e80556a43263e2906fabf3be15aca3c6674b727
TC Kuo <cot@cmdm.csie.ntu.edu.tw>
2023-02-03 12:56:25 +0800

    modify redierction if not sign in

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
     devise_parameter_sanitizer.permit(:account_update, keys: [:email])
   end
+  private
+    def authenticate_user!
+      if user_signed_in?
+        super
+      else
+        redirect_to '/users/sign_in', notice: "Please Login to view that page!"
+      end
+    end
 end
