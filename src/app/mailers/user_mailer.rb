f74de06008f8e95dcfd4cdc32299318893bc5a84
yuy@cmdm197 <yuy@cmdm.csie.ntu.edu.tw>
2023-02-19 13:01:56 +0000

    fixed bugs

--- src/app/mailers/user_mailer.rb
+++ src/app/mailers/user_mailer.rb
@@ -1,19 +1,17 @@
-mailer_domain = "lab@cmdm.csie.ntu.edu.tw"
-
 class UserMailer < Devise::Mailer
     def confirmation_instructions(record, token, opts={})
         headers["Custom-header"] = "Bar"
         opts[:subject] = 'Thank for your registration for FastEval Parkinsonism - account confirmation'
-        opts[:from] = mailer_domain
-        opts[:reply_to] = mailer_domain
+        opts[:from] = "lab@cmdm.csie.ntu.edu.tw"
+        opts[:reply_to] = "lab@cmdm.csie.ntu.edu.tw"
         super
     end
 
     def reset_password_instructions(record, token, opts={})
         headers["Custom-header"] = "Bar"
         opts[:subject] = 'Thank for your using FastEval Parkinsonism - password reset/change'
-        opts[:from] = mailer_domain
-        opts[:reply_to] = mailer_domain
+        opts[:from] = "lab@cmdm.csie.ntu.edu.tw"
+        opts[:reply_to] = "lab@cmdm.csie.ntu.edu.tw"
         super
     end
 end
